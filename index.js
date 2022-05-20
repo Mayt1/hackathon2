4105d35d249f8594d2bb4957a645b05cb03bab40
VictorUlian <yctor.ulian@gmail.com>
2022-05-20 16:51:33 -0300

    FIX: deploy teste1

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import { MongoClient, ObjectId } from "mongodb";
 
 dotenv.config();
-
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -12,9 +11,18 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 app.get("/teste", async (req, res) => {
     console.log("oi")
-    return res.status(200).send("tudo certo")
+    let nome = "to no banco mae"
+    try {
+        await mongoClient.connect()
+        const db = mongoClient.db(process.env.DATABASE);
+        await db.collection("users").insertOne({
+            nome: nome,
+        });
+        return res.status(201).send("Tudo certo")
+    }catch{
+        return res.status(404)
+    }
 });
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
