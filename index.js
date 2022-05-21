dd84e055963f3b9812af43ae6669b5f229dc83fa
VictorUlian <yctor.ulian@gmail.com>
2022-05-20 21:21:53 -0300

    FEAT: Now can get subjects and POST new question

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import bcrypt from "bcrypt"
 import dotenv from "dotenv";
-import jwt from "jsonwebtoken"
-
+import jwt from "jsonwebtoken";
+import dayjs from "dayjs";
 import { MongoClient, ObjectId } from "mongodb";
 
 import schemaUser from "./schemaUser.js";
@@ -15,9 +15,6 @@ app.use(cors());
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 await mongoClient.connect()
 const db = mongoClient.db(process.env.DATABASE);
-
-
-
 
 app.post('/signup', async (req, res) => {
     const { name, email, password, confirmPassword, isTeacher, school } = req.body;
@@ -66,6 +63,7 @@ app.post('/login', async (req, res) => {
             //console.log(sessao.insertedId)
             const sessionId = { session: sessao.insertedId };
             const secretKey = process.env.JWT_SECRET;
+
             const configurationJwt = {expiresIn: 60*60*24*30 } //30dias em segundos
             const token = jwt.sign(sessionId, secretKey, configurationJwt);
             await db.collection("sessions").updateOne({_id: sessao.insertedId}, {$set: {'token': token}})
@@ -81,6 +79,32 @@ app.post('/login', async (req, res) => {
     }
 });
 
+app.delete("/logout", async (req,res) => {
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+    const secretKey = process.env.JWT_SECRET;
+    if (!token) {
+            return res.status(401).send("Sem token");
+    } else {
+        try {
+            const sessionId = jwt.verify(token, secretKey);
+            await mongoClient.connect()
+            const db = mongoClient.db(process.env.DATABASE);
+            console.log(sessionId)
+            const del = await db.collection("sessions").deleteOne({_id:new ObjectId(sessionId.session)}, (err) => {
+                if(err) {
+                    return res.sendStatus(400)
+                } else {
+                    return res.sendStatus(204)
+                }
+            })
+        } catch (e) {
+            console.error("token invalido" + e);
+            return res.sendStatus(422);
+        }
+    }
+});
+
 app.post('/subjects', async(req,res) => {
     try{
         await db.collection('subjects').insertOne({
@@ -89,6 +113,17 @@ app.post('/subjects', async(req,res) => {
         res.sendStatus(201);
     }catch (e) {
         console.log('deu ruim');
+    }
+}) ;
+
+
+app.get('/subjects', async(req,res) => {
+    try{
+        const subjects = await db.collection('subjects').find().toArray();
+        res.status(200).send(subjects);
+    }catch (e) {
+        console.log(e);
+        return res._construct.sendStatus(500);
     }
 }) ;
 
@@ -115,10 +150,54 @@ async function validateUser(req,res,next){
     }
 }
 
-app.post("/questions", validateUser, async (req, res) => {
+app.post("/subject/:idSubject/questions", validateUser, async (req, res) => {
+    const {title, question} = req.body
+    const {idSubject} = req.params
+    if(!idSubject || !title || !question){
+        return res.status(422).send("Dados obrigatorios nao enviados")
+    }
     try {
-        
-         
+        const subject = await db.collection("subjects").findOne({_id: new ObjectId(idSubject)});
+        if (!subject) return res.status(404).send("Disciplina nao encontrada");
+        const {userData} = res.locals
+        let date = dayjs().format("YYYY-MM-DD HH:mm").toString()
+        await db.collection("questions").insertOne({
+            userId: userData._id,
+            username: userData.name,
+            subject: subject.subject,
+            title: title,
+            question: question,
+            createdDate: date,
+            answered: false
+        })
+        return res.status(201).send("")
+    } catch (e) {
+        console.error("token invalido" + e);
+        return res.sendStatus(422);
+    }
+});
+
+app.post("/questions/:id/answers", validateUser, async (req, res) => {
+    const {title, question} = req.body
+    const {idSubject} = req.params
+    if(!idSubject || !title || !question){
+        return res.status(422).send("Dados obrigatorios nao enviados")
+    }
+    try {
+        const subject = await db.collection("subjects").findOne({_id: new ObjectId(idSubject)});
+        if (!subject) return res.status(404).send("Disciplina nao encontrada");
+        const {userData} = res.locals
+        let date = dayjs().format("YYYY-MM-DD HH:mm").toString()
+        await db.collection("questions").insertOne({
+            userId: userData._id,
+            username: userData.name,
+            subject: subject.subject,
+            title: title,
+            question: question,
+            createdDate: date,
+            answered: false
+        })
+        return res.status(201).send("")
     } catch (e) {
         console.error("token invalido" + e);
         return res.sendStatus(422);
--- schemaUser.js
+++ schemaUser.js
@@ -1,11 +1,12 @@
 import Joi from "joi";
 
 const schemaUser = Joi.object({
-    name: Joi.string().uppercase().min(5).required(),
-    email: Joi.string().uppercase().email().min(1).max(50), 
+    name: Joi.string().min(5).required(),
+    email: Joi.string().email().min(1).max(50), 
     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
     confirmPassword: Joi.ref('password'),
-    isTeacher: Joi.valid(true, false).required()
+    isTeacher: Joi.valid(true, false).required(),
+    school: Joi.string()
   });
 
 export default schemaUser;
